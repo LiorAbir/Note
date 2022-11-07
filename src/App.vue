4293f9500e43d329f9fb18258598c4e94d5382ab
Lior Abir <liorabir909@gmail.com>
2022-11-07 18:18:05 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<h1>my-app</h1>
+	<RouterView />
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style></style>
---
+++ src/views/home-page.vue
@@ -0,0 +1,9 @@
+<template>
+	<section class="home-page">
+		<h1>home-page</h1>
+	</section>
+</template>
+<script>
+export default {}
+</script>
+<style lang=""></style>
