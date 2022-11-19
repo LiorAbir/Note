95b48fb35d9ba827f4d457b2737893f68a151a39
Lior Abir <liorabir909@gmail.com>
2022-11-19 23:35:10 +0200

    added home page btn scss

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -3,7 +3,6 @@
 		class="home-page"
 		:style="{ 'background-image': 'url(../assets/img/blob.svg)' }"
 	>
-		<!-- <img class="blob" src="../assets/img/blob.svg" alt="blob" /> -->
 		<header class="home-page-header flex">
 			<nav class="main-nav">
 				<router-link to="/note">Note</router-link>
@@ -13,14 +12,14 @@
 
 		<div class="home-page-content flex">
 			<div class="get-started flex">
-				<h1>note</h1>
+				<h1>noted .</h1>
 				<p>
 					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
 					Molestias, eius? Laboriosam quasi eveniet tempore incidunt, sed
 					quae fugiat veniam quas unde accusamus suscipit pariatur quaerat,
 					ducimus omnis sint. Iure, distinctio.
 				</p>
-				<button>Start</button>
+				<button class="start-btn">Start for free</button>
 			</div>
 			<img src="../assets/img/notesUndraw.svg" alt="note" />
 		</div>
