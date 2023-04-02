0eab145032db7ff17cc2f4bc63196825d9bb96f4
Lior Abir <liorabir909@gmail.com>
2023-04-02 09:57:30 +0300

    added content to home page

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -13,6 +13,20 @@
 				</router-link>
 			</div>
 		</div>
+		<div class="content capture flex">
+			<h2 class="title">Capture what's on your mind</h2>
+			<p class="sub-title">Add notes, lists, and photos to keep.</p>
+			<div class="container flex">
+				<div class="notes flex">
+					<div class="img"></div>
+					<div class="sub-img"></div>
+				</div>
+				<div class="lists flex">
+					<div class="img"></div>
+					<div class="sub-img"></div>
+				</div>
+			</div>
+		</div>
 	</section>
 </template>
 <script>
